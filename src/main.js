2054ad5f979d167bf2a2ee88653d3c7912ed85ad
1562841176 <1562841176@qq.com>
2018-12-29 17:11:44 +0800

    添加路径及vuex

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 
 import App from './App'
+import Element from 'element-ui'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
+Vue.use(Element)
+
 new Vue({
   el: '#app',
+  router,
+  store,
   render: h => h(App)
 })
