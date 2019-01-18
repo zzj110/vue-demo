2dccab4e447ab2c7585cce8081bba19b930646d2
1562841176 <1562841176@qq.com>
2019-01-18 14:37:03 +0800

    骨架屏，存在bug

---
+++ skeleton.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const { resolve } = require('path')
+const htmlMinifier = require('html-minifier')
+const createBundleRenderer = require('vue-server-renderer').createBundleRenderer
+
+// 先把vue的模板文件index.html置换成标准的模板，防止骨架屏污染
+const tempData = fs.readFileSync(resolve(__dirname, './template.html'), 'utf-8')
+fs.writeFileSync('./index.html', tempData, 'utf-8')
+console.log('模板注入完成')
+// 读取`skeleton.json`，以`index.html`为模板写入内容
+const renderer = createBundleRenderer(resolve(__dirname, './dist/skeleton.json'), {
+  template: fs.readFileSync(resolve(__dirname, './index.html'), 'utf-8')
+})
+console.log('模板注入完成11')
+
+// 把上一步模板完成的内容写入（替换）`index.html`
+renderer.renderToString({}, (err, html) => {
+  if (err) {
+    console.log(err)
+    return
+  }
+  html = htmlMinifier.minify(html, {
+    collapseWhitespace: true,
+    minifyCSS: true
+  })
+  fs.writeFileSync('./index.html', html, 'utf-8')
+})
+console.log('骨架屏注入完成')
---
+++ src/Skeleton.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="skeleton page">
+    <div class="skeleton-nav"/>
+    <div class="skeleton-swiper">
+      <div class="skeleton-swiper-item item-one"/>
+      <div class="skeleton-swiper-item item-two"/>
+    </div>
+  </div>
+</template>
+
+<style>
+html,body,div{
+    margin:0;
+    padding:0;
+}
+.skeleton {
+  height: 100%;
+  overflow: hidden;
+  box-sizing: border-box;
+  background: #fff;
+}
+.skeleton-nav {
+  height: 54px;
+  background: #eee;
+  margin-bottom: 20px;
+}
+.skeleton-swiper {
+    min-height:600px;
+    max-width:1280px;
+    margin:0 auto;
+}
+.skeleton-swiper-item{
+    min-height: 600px;
+    height:100%;
+    background:#eee;
+    border-radius:5px;
+}
+.item-one{
+    width:20%;
+    float:left;
+}
+.item-two{
+    width:78%;
+    float:right;
+}
+</style>
